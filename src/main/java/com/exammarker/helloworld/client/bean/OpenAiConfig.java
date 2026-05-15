0a3d14da9e45f07dc4e92bc7209afe4d61ac622f
hammadquddus <hammadquddus@hammadqudduss-MacBook-Pro.local>
2026-05-15 11:23:09 +0500

    OpenAiConfig

--- src/main/java/com/exammarker/helloworld/client/bean/OpenAiConfig.java
+++ src/main/java/com/exammarker/helloworld/client/bean/OpenAiConfig.java
@@ -1,6 +1,7 @@
 package com.exammarker.helloworld.client.bean;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 public class OpenAiConfig {
 
     @Bean
-    public ChatClient chatClient(ChatClient.Builder builder) {
-        return builder.build();
+    public ChatClient chatClient(OpenAiChatModel model) {
+        return ChatClient.builder(model).build();
     }
 }
